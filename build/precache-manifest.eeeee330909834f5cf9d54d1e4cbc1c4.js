7e81d6ffde9e78ed8991f3df8b2aefb8459e8fd5
Hugo A. Pantoja <hugoandres272@gmail.com>
2020-12-30 01:03:38 -0500

    Añadiendo construcción sin añadirlo a gitignore

---
+++ build/precache-manifest.eeeee330909834f5cf9d54d1e4cbc1c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0059b646fa501d607947f475b11ef179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a479ea93876bf5f207d",
+    "url": "/static/css/main.8d98ee4e.chunk.css"
+  },
+  {
+    "revision": "229b46f8cda116b3961b",
+    "url": "/static/js/2.a75147b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a75147b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a479ea93876bf5f207d",
+    "url": "/static/js/main.867bf2af.chunk.js"
+  },
+  {
+    "revision": "b6d2f227e4a48dad9472",
+    "url": "/static/js/runtime-main.dda23e0c.js"
+  }
+]);
