f7ede70f41682b96bd8f30a998e426b9f0cc0479
Hugo A. Pantoja <hugoandres272@gmail.com>
2020-12-30 16:26:49 -0500

    Updates

---
+++ precache-manifest.68027fb48905e50886bb2c72ba03d1dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c574cc2a36874a0e6ac0d1878c4117a",
+    "url": "/PaginaDeVentas/index.html"
+  },
+  {
+    "revision": "9ac942483cbe6f52627d",
+    "url": "/PaginaDeVentas/static/css/main.8d98ee4e.chunk.css"
+  },
+  {
+    "revision": "229b46f8cda116b3961b",
+    "url": "/PaginaDeVentas/static/js/2.a75147b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PaginaDeVentas/static/js/2.a75147b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac942483cbe6f52627d",
+    "url": "/PaginaDeVentas/static/js/main.53523214.chunk.js"
+  },
+  {
+    "revision": "8e66e090bf68d3a4c1e6",
+    "url": "/PaginaDeVentas/static/js/runtime-main.f954a4fc.js"
+  }
+]);
