cad6b0ff79ecbad39c2288961a0aff1f6b8be2e7
Hugo A. Pantoja <hugoandres272@gmail.com>
2021-01-01 15:49:42 -0500

    Uso de basename y exact

--- src/App.js
+++ src/App.js
@@ -59,37 +59,37 @@ function App() {
                 <Route exact path="/">
                   <Inicio />
                 </Route>
-                <Route path="/mouse">
+                <Route exact path="/mouse">
                   <Main title="Mouse" url={urlMouse} list={setProducto}/>
                 </Route>
-                <Route path="/teclados">
+                <Route exact path="/teclados">
                   <Main title="Teclados" url={urlTeclados} list={setProducto} />
                 </Route>
-                <Route path="/memorias">
+                <Route exact  path="/memorias">
                   <Main title='Memorias' url={urlMemorias} list={setProducto} />
                 </Route>
-                <Route path="/audifonos">
+                <Route exact path="/audifonos">
                   <Main title="Audífonos" url={urlAudifonos} list={setProducto} />
                 </Route>
-                <Route path="/carro">
+                <Route exact path="/carro">
                   <Carro />
                 </Route>
-                <Route path="/productos/mouse/:Id">
+                <Route exact path="/productos/mouse/:Id">
                   <ProductoMoreInfo list={setProducto} />
                 </Route>
-                <Route path="/productos/teclados/:Id">
+                <Route exact path="/productos/teclados/:Id">
                   <ProductoMoreInfo list={setProducto} />
                 </Route>
-                <Route path="/productos/memorias/:Id">
+                <Route exact path="/productos/memorias/:Id">
                   <ProductoMoreInfo list={setProducto} />
                 </Route>
-                <Route path="/productos/audifonos/:Id">
+                <Route exact path="/productos/audifonos/:Id">
                   <ProductoMoreInfo list={setProducto} />
                 </Route>
-                <Route path="/perfil">
+                <Route exact path="/perfil">
                   <Perfil />
                 </Route>
-                <Route path="/pedidos">
+                <Route exact path="/pedidos">
                   <Pedidos />
                 </Route>
             </Switch>
@@ -101,8 +101,8 @@ function App() {
   }else if(state === 2){
     return (
       <>
-        <Router>
-          <Route path="/cuenta">
+        <Router basename={process.env.PUBLIC_URL}>
+          <Route exact path="/cuenta">
             <Cuenta />
           </Route>
         </Router>
@@ -111,8 +111,8 @@ function App() {
   }else if(state === 3){
     return (
       <>
-        <Router>
-          <Route path="/carro">
+        <Router basename={process.env.PUBLIC_URL}>
+          <Route exact path="/carro">
             <Carro />
           </Route>
         </Router>
@@ -121,8 +121,8 @@ function App() {
   }else if(state === 4){
     return (
       <>
-        <Router>
-          <Route path="/nuevoCliente">
+        <Router basename={process.env.PUBLIC_URL}>
+          <Route exact path="/nuevoCliente">
             <NuevoCliente />
           </Route>
         </Router>
