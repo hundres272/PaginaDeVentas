c7739ea149e6791fc489356d9413c066607cbff8
Hugo A. Pantoja <hugoandres272@gmail.com>
2021-01-11 01:57:28 -0500

    Updates

--- precache-manifest.452a4093e863b1cb5f12cfa8e88d0f45.js
+++ precache-manifest.452a4093e863b1cb5f12cfa8e88d0f45.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dc93979f97398bfb2529c0d792b08e2c",
+    "revision": "9e75ada30e26216f21ac6608a4311092",
     "url": "/PaginaDeVentas/index.html"
   },
   {
-    "revision": "792a41974b25d04cbb49",
+    "revision": "4843925ecb82387f5a14",
     "url": "/PaginaDeVentas/static/css/main.8d98ee4e.chunk.css"
   },
   {
-    "revision": "5afe42a9a5598aa0f910",
-    "url": "/PaginaDeVentas/static/js/2.35dffe9f.chunk.js"
+    "revision": "3cf5064554c7a92a2f13",
+    "url": "/PaginaDeVentas/static/js/2.e1bb3697.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/PaginaDeVentas/static/js/2.35dffe9f.chunk.js.LICENSE.txt"
+    "url": "/PaginaDeVentas/static/js/2.e1bb3697.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "792a41974b25d04cbb49",
-    "url": "/PaginaDeVentas/static/js/main.147fd83c.chunk.js"
+    "revision": "4843925ecb82387f5a14",
+    "url": "/PaginaDeVentas/static/js/main.92ea3282.chunk.js"
   },
   {
     "revision": "8e66e090bf68d3a4c1e6",
